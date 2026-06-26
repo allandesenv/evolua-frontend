c6c26b284e3b299175fc27e2f0534a32c4deaca4
allandesenv <allandd8@gmail.com>
2026-06-25 22:41:10 -0300

    adicionado logs para oauth

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -5,7 +5,8 @@ class AppConfig {
 
   static const apiBaseUrl = String.fromEnvironment(
     'EVOLUA_API_BASE_URL',
-    defaultValue: 'https://evolua-api-production.up.railway.app',
+    defaultValue: 'https://evolua-api-hml-production.up.railway.app',
+    //defaultValue: 'https://evolua-api-production.up.railway.app',
     //defaultValue: 'http://192.168.0.47:8080',
   );
 
